e3e5bce43adc51c90d851f0c4b5dd47275864868
JakobWoelfer <jakob.woelfer@gmail.com>
2025-05-14 21:47:13 +0200

    #4: generate your-profile component in auth module

---
+++ frontend/src/app/modules/auth/pages/your-profile/your-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-profile',
+  imports: [],
+  templateUrl: './your-profile.component.html',
+  styleUrl: './your-profile.component.css'
+})
+export class YourProfileComponent {
+
+}
