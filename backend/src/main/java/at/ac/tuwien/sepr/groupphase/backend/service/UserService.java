c202e7c5c91241f7f0aef5e062208a8745b092b0
JakobWoelfer <jakob.woelfer@gmail.com>
2025-05-14 19:19:36 +0200

    4: add method to UserService for changing password of the current user

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
+import at.ac.tuwien.sepr.groupphase.backend.service.dto.ChangePasswordDto;
 import at.ac.tuwien.sepr.groupphase.backend.service.dto.UserDataDto;
 import at.ac.tuwien.sepr.groupphase.backend.service.dto.UserRoleDto;
 import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
@@ -20,6 +21,14 @@ public interface UserService {
      * @throws NotFoundException when the user does not exist
      */
     void assignRoleToUser(UserRoleDto userRole) throws NotFoundException;
+
+    /**
+     * Change the password of the currently authenticated user.
+     *
+     * @param dto contains the old and new password
+     */
+    void changePasswordOfCurrentUser(ChangePasswordDto dto);
+
 
     /**
      * Create a new user with a given email and password. The email must not be used by any other user yet.
