4fbd7484154061b37f9a9ac425037ace156784ca
S1mple133 <pavelescu_darius@yahoo.com>
2025-05-24 23:05:33 +0200

    #45: add check to inviteEmployee method of department user management in frontend

--- frontend/src/app/modules/departments/components/department-user-management/department-user-management.component.ts
+++ frontend/src/app/modules/departments/components/department-user-management/department-user-management.component.ts
@@ -48,6 +48,11 @@ export class DepartmentUserManagementComponent implements OnChanges, OnInit {
   }
 
   inviteEmployee() {
+    if(!this.newEmployeeMail || !this.newEmployeeMail.trim()) {
+      this.toastrService.error("Please enter a valid Employee Mail", "Error occurred");
+      return;
+    }
+
     this.departmentService.addEmployeeToDepartment(this.departmentName, this.newEmployeeMail).subscribe({
       next: (employee) => {
         this.toastrService.success('Successfully added ' + employee.email + ' to ' + employee.departmentName +'!');
